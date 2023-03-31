c63a59e7039c95f5c5074cceb3f32f53b78be0d5
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-03-31 23:38:56 +0530

    learned to merge two c files

---
+++ login.h
@@ -0,0 +1,11 @@
+#ifndef LOGIN_DOT_H
+#define LOGIN_DOT_H
+
+struct login
+{
+    char Name[50];
+    char Username[50];
+    char Password[50];
+};
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-
+#include "login.h"
+struct login m = {"Swastik", "form", "123456"};
 int main()
 {
-    
+    printf("%s\n%s\n%s\n", m.Name, m.Username, m.Password);
     return 0;
 }
