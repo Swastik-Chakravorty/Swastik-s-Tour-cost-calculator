ef3a93277650e66e9d9c49a19864aa322f190d88
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-04-01 23:22:22 +0530

    added new file for destination details

---
+++ Destination_Details.h
@@ -0,0 +1,10 @@
+#ifndef DESTINATION_DETAILS_DOT_H
+#define DESTINATION_DETAILS_DOT_H
+#include <stdio.h>
+#include <string.h>
+
+void work(void){
+    printf("\n\nWill be added soon\n\n");
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include "login.h"
+#include "Login.h"
+#include "Destination_Details.h"
 
 int main()
 {
     printf(TXTGRN "\e[1m\n\t\t--------------WELCOME TO SWASTIK'S TOUR COST CALCULATOR------------\n\e[m" TXTRST);
 
     login_input();
+    work();
 
     return 0;
 }
