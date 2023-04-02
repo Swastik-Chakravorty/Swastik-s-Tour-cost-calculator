22764ac88f8bbebf55a85dea341818ef24f78497
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-04-02 15:47:58 +0530

    get input with condition but stuck in comparison

--- Destination_Details.c
+++ Destination_Details.c
@@ -3,10 +3,24 @@
 #include <stdio.h>
 #include <string.h>
 
+struct tour
+{
+    char source[30];
+    char destination[30];
+    char mode[10];
+    char sightseeing[50];
+};
+
+
 // void travel(){
     int main(){
     int usr_src, usr_dst;
     int k = 0;
+    struct tour user;
+    user.source[0] = '\0';
+    user.destination[0] = '\0';
+    user.mode[0] = '\0';
+    user.sightseeing[0] = '\0';
     char src_stn[12][15] = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose", "Boulder", "Virginia"};
     char dst_stn[12][30] = {"New York City", "Los Angeles", "Chicago", "Washington, D.C.", "Las Vegas", "San Francisco", "New Orleans", "Asheville, North Carolina", "Hawaii", "Sedona and the Grand Canyon", "Florida", "Big Sur"};
 
@@ -40,9 +54,19 @@
         k += 4;
         printf("\n");
     }
-    printf("\nYour Source Station by number: ");
-    scanf("%d", &usr_src);
-    fgetc(stdin);
+    for (int l = 0; l < 12; l++)
+    {
+        do
+        {
+            printf("\nYour Source Station: ");
+            fgets(user.source, 30, stdin);
+        } while (strcmp(src_stn[l], user.source) != 1);
+        break;
+    }
+    printf("You Choose: %s", user.source);
+    
+    // fgetc(stdin);
+    
     
 }
 
