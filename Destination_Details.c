8c27e14ec0848d20c5a64e7194c4aa8c8be3ead9
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-04-02 14:31:45 +0530

    get input from user

--- Destination_Details.c
+++ Destination_Details.c
@@ -5,11 +5,13 @@
 
 // void travel(){
     int main(){
+    int usr_src, usr_dst;
     int k = 0;
     char src_stn[12][15] = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose", "Boulder", "Virginia"};
     char dst_stn[12][30] = {"New York City", "Los Angeles", "Chicago", "Washington, D.C.", "Las Vegas", "San Francisco", "New Orleans", "Asheville, North Carolina", "Hawaii", "Sedona and the Grand Canyon", "Florida", "Big Sur"};
 
-    printf("Plaese choose your Source Station by number: ");
+    printf("\n\nPlaese choose your Source Station by number: ");
+    printf("\n\n");
     for (int i =0; i < 3; i++)
     {
         for (int j = k; j < k+4; j++)
@@ -38,15 +40,10 @@
         k += 4;
         printf("\n");
     }
-    // char usrName[50], pWord[9], pwd2;
-    // printf(TXTYLW "\nPlease choose your operation:\n");
-    // printf("1: Signup\n");
-    // printf("2: Login\n");
-    // printf("3: Exit\n");
-
-    // printf("\nYour choice: " TXTRST);
-    // scanf("%d", &opt);
-    // fgetc(stdin);
+    printf("\nYour Source Station by number: ");
+    scanf("%d", &usr_src);
+    fgetc(stdin);
+    
 }
 
 // #endif
--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "Login.h"
-#include "Destination_Details.h"
+// #include "Destination_Details.h"
 // #include "Travel_Mode.h"
 // #include "Tour_Details.h"
 
@@ -8,12 +8,13 @@ int main()
 {
     printf(TXTGRN "\e[1m\n\t\t--------------WELCOME TO SWASTIK'S TOUR COST CALCULATOR------------\n\e[m" TXTRST);
 
-    int a;
-    a = login_input();
-    if (a ==  1)
-    {
-        travel();
-    }
+    // int a;
+    // a = 
+    login_input();
+    // if (a ==  1)
+    // {
+    //     travel();
+    // }
 
     return 0;
 }
