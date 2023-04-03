bc2275b0c6fcae3a438dff30a17a61174d4a0976
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-04-03 15:56:43 +0530

    set Destination function

--- Destination_Details.c
+++ Destination_Details.c
@@ -6,21 +6,46 @@
 struct tour
 {
     char source[50];
-    char destination[30];
+    char destination[50];
     char mode[10];
     char sightseeing[50];
 };
 
+void destinations(char dst[12][50], struct tour usr){
+    for (int i = 0; i < 12; i++)
+    {
+        int y = 0;
+        printf("\n\nYour Destination Station: ");
+        gets(usr.destination);
+
+        for (int j = 0; j < 12; j++)
+        {
+            if (strcmp(dst[j], usr.destination) == 0)
+            {
+                printf("%s\n", dst[j]);
+                printf("%s\n", usr.destination);
+                printf("Mached\n");
+                printf("You Choose: %s\n", usr.destination);
+                y = 1;
+                break;
+            }
+        }
+
+        if (y == 1)
+        {
+            break;
+        }
+    }
+}
+
 // void travel(){
 int main()
 {
-    int usr_src, usr_dst;
     int k = 0;
     struct tour user;
-    int x = 0;
 
     char src_stn[12][50] = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose", "Boulder", "Virginia"};
-    char dst_stn[12][30] = {"New York City", "Los Angeles", "Chicago", "Washington, D.C.", "Las Vegas", "San Francisco", "New Orleans", "Asheville, North Carolina", "Hawaii", "Sedona and the Grand Canyon", "Florida", "Big Sur"};
+    char dst_stn[12][50] = {"New York City", "Los Angeles", "Chicago", "Washington, D.C.", "Las Vegas", "San Francisco", "New Orleans", "Asheville, North Carolina", "Hawaii", "Sedona and the Grand Canyon", "Florida", "Big Sur"};
 
     printf("\n\nPlaese choose your Source Station (** You will have only 12 chances - Please write the name coorectly as mentioned below**): ");
     printf("\n\n");
@@ -61,17 +86,20 @@ int main()
 
     for (int l = 0; l < 12; l++)
     {
+        int x = 0;
         printf("\nYour Source Station: ");
         gets(user.source);
 
-        for (int l = 0; l < 12; l++)
+        for (int m = 0; m < 12; m++)
         {
-            if (strcmp(src_stn[l], user.source) == 0)
+            if (strcmp(src_stn[m], user.source) == 0)
             {
-                printf("%s\n", src_stn[l]);
-                printf("%s\n", user.source);
-                printf("Mached\n");
+                // printf("%s\n", src_stn[m]);
+                // printf("%s\n", user.source);
+                // printf("Mached\n");
                 printf("You Choose: %s\n", user.source);
+                destinations(dst_stn, user);
+
                 x = 1;
                 break;
             }
