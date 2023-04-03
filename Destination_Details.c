d426693fe1c1f7dfca4de65ac1f62bf8ef5da12a
Swastik Chakravorty <swastik.chakravorty@gmail.com>
2023-04-03 16:17:22 +0530

    Destination function working fine

--- Destination_Details.c
+++ Destination_Details.c
@@ -2,6 +2,7 @@
 // #define DESTINATION_DETAILS_DOT_H
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
 
 struct tour
 {
@@ -12,19 +13,59 @@ struct tour
 };
 
 void destinations(char dst[12][50], struct tour usr){
-    for (int i = 0; i < 12; i++)
+    system("cls");
+    int b = 0;
+    printf("\n\nPlaese choose your Destination Station (** You will have only 12 chances - Please write the name coorectly as mentioned below**): ");
+    printf("\n\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = b; j < b + 4; j++)
+        {
+            printf("%d: %s", (j + 1), dst[j]);
+            printf("       ");
+            if (j == 4)
+            {
+                printf("    ");
+            }
+            else if (j == 5)
+            {
+                printf("\b\b");
+            }
+            else if (j == 6)
+            {
+                printf("\b\b\b\b");
+            }
+            else if (j == 8)
+            {
+                printf("      ");
+            }
+            else if (j == 9)
+            {
+                printf("   ");
+            }
+            else if (j == 10)
+            {
+                printf("\b");
+            }
+        }
+        b += 4;
+        printf("\n");
+    }
+
+    for (int l = 0; l < 12; l++)
     {
         int y = 0;
         printf("\n\nYour Destination Station: ");
         gets(usr.destination);
 
-        for (int j = 0; j < 12; j++)
+        for (int m = 0; m < 12; m++)
         {
-            if (strcmp(dst[j], usr.destination) == 0)
+            if (strcmp(dst[m], usr.destination) == 0)
             {
-                printf("%s\n", dst[j]);
+                printf("%s\n", dst[m]);
                 printf("%s\n", usr.destination);
                 printf("Mached\n");
+                printf("%s\n", usr.source);
                 printf("You Choose: %s\n", usr.destination);
                 y = 1;
                 break;
@@ -41,17 +82,17 @@ void destinations(char dst[12][50], struct tour usr){
 // void travel(){
 int main()
 {
-    int k = 0;
+    int a = 0;
     struct tour user;
 
     char src_stn[12][50] = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", "San Antonio", "San Diego", "Dallas", "San Jose", "Boulder", "Virginia"};
-    char dst_stn[12][50] = {"New York City", "Los Angeles", "Chicago", "Washington, D.C.", "Las Vegas", "San Francisco", "New Orleans", "Asheville, North Carolina", "Hawaii", "Sedona and the Grand Canyon", "Florida", "Big Sur"};
+    char dst_stn[12][50] = {"New York City", "Los Angeles", "Chicago", "Sedona and the Grand Canyon", "Las Vegas", "San Francisco", "New Orleans", "Asheville, North Carolina", "Hawaii", "Big Sur", "Florida", "Washington, D.C."};
 
     printf("\n\nPlaese choose your Source Station (** You will have only 12 chances - Please write the name coorectly as mentioned below**): ");
     printf("\n\n");
     for (int i = 0; i < 3; i++)
     {
-        for (int j = k; j < k + 4; j++)
+        for (int j = a; j < a + 4; j++)
         {
             printf("%d: %s", (j + 1), src_stn[j]);
             printf("               ");
@@ -69,7 +110,7 @@ int main()
             }
             else if (j == 8)
             {
-                printf("  ");
+                printf(" ");
             }
             else if (j == 9)
             {
@@ -80,7 +121,7 @@ int main()
                 printf("\b");
             }
         }
-        k += 4;
+        a += 4;
         printf("\n");
     }
 
